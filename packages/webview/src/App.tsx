e1753810e11939f517d336937761e1505d79c729
lianglonghui <lianglonghui@baidu.com>
2026-06-15 12:16:19 +0800

    chore: :hammer: 统一settings和tree位置

--- packages/webview/src/App.tsx
+++ packages/webview/src/App.tsx
@@ -4,8 +4,8 @@
 
 import { useWebviewBootstrap } from '@/bridge/use-webview-bootstrap';
 import { ChatApp } from '@/surfaces/chat/ChatApp';
-import { SettingsApp } from '@/settings/SettingsApp';
-import { TreeApp } from '@/tree/TreeApp';
+import { SettingsApp } from '@/surfaces/settings/SettingsApp';
+import { TreeApp } from '@/surfaces/tree/TreeApp';
 
 function App() {
   const surface = useWebviewBootstrap();
